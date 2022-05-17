cfcb5d92f409536d985330e9a6ab7ddaf0a47f90
J-P Nurmi <jp.nurmi@canonical.com>
2022-05-17 09:28:15 +0200

    Mock resource tests

--- test/lxd_test.dart
+++ test/lxd_test.dart
@@ -303,11 +303,6 @@ class MockLxdServer {
     } else if (request.method == 'GET' &&
         path.length == 2 &&
         path[0] == '1.0' &&
-        path[1] == 'resources') {
-      _getResources(response);
-    } else if (request.method == 'GET' &&
-        path.length == 2 &&
-        path[0] == '1.0' &&
         path[1] == 'certificates') {
       _getCertificates(response);
     } else if (request.method == 'GET' &&
@@ -473,97 +468,6 @@ class MockLxdServer {
       'public': false,
       'auth_methods': ['tls'],
       'environment': {}
-    });
-  }
-
-  void _getResources(HttpResponse response) {
-    _writeSyncResponse(response, {
-      'cpu': {'architecture': 'arm64'},
-      'memory': {'used': 4125806592, 'total': 17179869184},
-      'gpu': {
-        'cards': [
-          {
-            'driver': 'i915',
-            'driver_version': '5.8.0-36-generic',
-            'vendor': 'Intel Corporation',
-            'vendor_id': '8086'
-          }
-        ]
-      },
-      'network': {
-        'cards': [
-          {
-            'driver': 'atlantic',
-            'driver_version': '5.8.0-36-generic',
-            'vendor': 'Aquantia Corp.',
-            'vendor_id': '1d6a'
-          }
-        ]
-      },
-      'storage': {
-        'disks': [
-          {
-            'id': 'nvme0n1',
-            'model': 'INTEL SSDPEKKW256G7',
-            'serial': 'BTPY63440ARH256D',
-            'size': 256060514304,
-            'type': 'nvme'
-          }
-        ]
-      },
-      'usb': {
-        'devices': [
-          {
-            'bus_address': 1,
-            'device_address': 3,
-            'product': 'Hermon USB hidmouse Device',
-            'product_id': '2221',
-            'speed': 12,
-            'vendor': 'ATEN International Co., Ltd',
-            'vendor_id': '0557'
-          }
-        ]
-      },
-      'pci': {
-        'devices': [
-          {
-            'driver': 'mgag200',
-            'driver_version': '5.8.0-36-generic',
-            'pci_address': '0000:07:03.0',
-            'product': 'MGA G200eW WPCM450',
-            'product_id': '0532',
-            'vendor': 'Matrox Electronics Systems Ltd.',
-            'vendor_id': '102b'
-          }
-        ]
-      },
-      'system': {
-        'uuid': 'SYSTEM-UUID',
-        'vendor': 'SYSTEM-VENDOR',
-        'product': 'SYSTEM-PRODUCT',
-        'family': 'SYSTEM-FAMILY',
-        'version': '1.0',
-        'sku': 'SYSTEM-SKU',
-        'serial': 'SYSTEM-SERIAL',
-        'type': 'physical',
-        'firmware': {
-          'date': '10/08/2021',
-          'vendor': 'FIRMWARE-VENDOR',
-          'version': '1.0'
-        },
-        'chassis': {
-          'serial': 'CHASSIS-SERIAL',
-          'type': 'Notebook',
-          'vendor': 'CHASSIS-VENDOR',
-          'version': '1.0'
-        },
-        'motherboard': {
-          'product': 'MOTHERBOARD-PRODUCT',
-          'serial': 'MOTHERBOARD-SERIAL',
-          'vendor': 'MOTHERBOARD-VENDOR',
-          'version': '1.0'
-        }
-      }
     });
   }
 
@@ -862,36 +766,6 @@ class MockLxdServer {
 }
 
 void main() {
-  test('get resources', () async {
-    var lxd = MockLxdServer();
-    await lxd.start();
-
-    var client = LxdClient(socketPath: lxd.socketPath);
-    var resources = await client.getResources();
-    expect(resources.cpu.architecture, equals('arm64'));
-    expect(resources.memory.used, equals(4125806592));
-    expect(resources.memory.total, equals(17179869184));
-    expect(resources.system.uuid, equals('SYSTEM-UUID'));
-    expect(resources.system.firmware.date, equals('10/08/2021'));
-    expect(resources.system.firmware.vendor, equals('FIRMWARE-VENDOR'));
-    expect(resources.system.firmware.version, equals('1.0'));
-    expect(resources.system.chassis.serial, equals('CHASSIS-SERIAL'));
-    expect(resources.system.chassis.type, equals('Notebook'));
-    expect(resources.system.chassis.vendor, equals('CHASSIS-VENDOR'));
-    expect(resources.system.chassis.version, equals('1.0'));
-    expect(resources.system.motherboard.product, equals('MOTHERBOARD-PRODUCT'));
-    expect(resources.system.motherboard.serial, equals('MOTHERBOARD-SERIAL'));
-    expect(resources.system.motherboard.vendor, equals('MOTHERBOARD-VENDOR'));
-    expect(resources.system.motherboard.version, equals('1.0'));
-    expect(
-        resources.toString(),
-        equals(
-            'LxdResources(cpu: LxdCpuResources(architecture: arm64, sockets: []), memory: LxdMemoryResources(used: 4125806592, total: 17179869184), gpuCards: [LxdGpuCard(driver: i915, driverVersion: 5.8.0-36-generic, vendor: Intel Corporation, vendorId: 8086)], networkCards: [LxdNetworkCard(driver: atlantic, driverVersion: 5.8.0-36-generic, vendor: Aquantia Corp., vendorId: 1d6a)], storageDisks: [LxdStorageDisk(id: nvme0n1, model: INTEL SSDPEKKW256G7, serial: BTPY63440ARH256D, size: 256060514304, type: nvme)], usbDevices: [LxdUsbDevice(busAddress: 1, deviceAddress: 3, product: Hermon USB hidmouse Device, productId: 2221, speed: 12.0, vendor: ATEN International Co., Ltd, vendorId: 0557)], pciDevices: [LxdPciDevice(driver: mgag200, driverVersion: 5.8.0-36-generic, pciAddress: 0000:07:03.0, product: MGA G200eW WPCM450, productId: 0532, vendor: Matrox Electronics Systems Ltd., vendorId: 102b)], system: LxdSystemResources(uuid: SYSTEM-UUID, vendor: SYSTEM-VENDOR, product: SYSTEM-PRODUCT, family: SYSTEM-FAMILY, version: 1.0, sku: SYSTEM-SKU, serial: SYSTEM-SERIAL, type: physical, firmware: LxdFirmware(date: 10/08/2021, vendor: FIRMWARE-VENDOR, version: 1.0), chassis: LxdChassis(serial: CHASSIS-SERIAL, type: Notebook, vendor: CHASSIS-VENDOR, version: 1.0), motherboard: LxdMotherboard(product: MOTHERBOARD-PRODUCT, serial: MOTHERBOARD-SERIAL, vendor: MOTHERBOARD-VENDOR, version: 1.0)))'));
-
-    client.close();
-    await lxd.close();
-  });
-
   test('get certificates', () async {
     var lxd = MockLxdServer(certificates: {
       '213394bb': MockCertificate(),
