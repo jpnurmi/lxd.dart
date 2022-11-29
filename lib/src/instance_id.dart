7c228617193af16477a51f1e8818f95652fae834
J-P Nurmi <jp.nurmi@canonical.com>
2022-11-29 11:38:41 +0100

    'default' project

--- lib/src/instance_id.dart
+++ lib/src/instance_id.dart
@@ -2,44 +2,43 @@ import 'package:collection/collection.dart';
 import 'package:meta/meta.dart';
 
 import 'api/instance.dart';
+import 'constants.dart';
 
 /// Identifies an LXD instance
 @immutable
 class LxdInstanceId {
-  const LxdInstanceId(this.name, {this.project});
+  const LxdInstanceId(this.name, {this.project = kLxdDefaultProject});
 
   /// Name associated with the instance
   final String name;
 
   /// Project associated with the instance
-  final String? project;
+  final String project;
 
   factory LxdInstanceId.fromPath(String path) {
     final uri = Uri.tryParse(path);
     return LxdInstanceId(
       uri?.pathSegments.lastOrNull ?? path.split('/').last,
-      project: uri?.queryParameters['project'],
+      project: uri?.queryParameters['project'] ?? kLxdDefaultProject,
     );
   }
 
   @override
-  int get hashCode => Object.hash(name, project ?? '');
+  int get hashCode => Object.hash(name, project);
 
   @override
   bool operator ==(Object other) {
     if (identical(this, other)) return true;
     return other is LxdInstanceId &&
         other.name == name &&
-        // null and empty are considered equal
-        (other.project ?? '') == (project ?? '');
+        other.project == project;
   }
 
   @override
   String toString() {
     return Uri(
       path: name,
-      queryParameters:
-          project?.isNotEmpty == true ? {'project': project} : null,
+      queryParameters: {'project': project},
     ).toString();
   }
 }
